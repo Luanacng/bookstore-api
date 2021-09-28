2571ca83ae97fcc61223716cd2c0b10abeef573c
Luanacng <44030428+Luanacng@users.noreply.github.com>
2021-09-28 17:52:37 -0300

    repositories implementados

---
+++ src/main/java/com/luana/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.luana.bookstore.repositories;
+
+import com.luana.bookstore.domain.Categoria;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/luana/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.luana.bookstore.repositories;
+
+import com.luana.bookstore.domain.Livro;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+}
